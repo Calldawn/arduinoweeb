05f3989855e775c353f3d47cb909de9e88382fb2
milliax <wei@sivir.pw>
2022-03-24 07:33:31 +0000

    update sever things

--- server/index.js
+++ server/index.js
@@ -1,12 +1,43 @@
+require("dotenv").config()
 const express = require("express")
+const linebot = require("linebot")
+const bodyParser = require("body-parser")
 const app = express()
-
 const port = 8000
 
-app.get("/",(res,req)=>{
-    return req.send("Hello World")
+const bot = linebot({
+    channelId: process.env.LINE_BOT_ID,
+    channelSecret: process.env.LINE_BOT_SECRET,
+    channelAccessToken: process.env.LINE_BOT_ACCESS_TOKEN
 })
 
-app.listen(port,()=>{
+const linebotParser = bot.parser()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+/* Testing */
+app.post("/broadcast", (req, res) => {
+    try {
+        bot.broadcast(req.body.message)
+        return res.sendStatus(200).send("Message sent")
+    }catch(err){
+        return res.sendStatus(403).send(`Error: ${err}`)
+    }
+})
+
+app.listen(port, () => {
     console.log("Server Started")
+})
+
+
+bot.on("message",event=>{
+    const message = event.message.text;
+    console.log(`Recieved: ${message}`)
+    try{
+        event.reply(`Hello, Nice to meet you.`)
+        console.log("message replied")
+    }catch(err){
+        console.log("message reply failed")
+    }
 })
